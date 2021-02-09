bade2a000e17bb7a2647eb9686114af399b9b612
James Wilson <james@jsdw.me>
2021-02-09 12:50:41 +0000

    add a manifest for more applike experience in some places

--- client/src/components/Login.svelte
+++ client/src/components/Login.svelte
@@ -51,8 +51,11 @@
 
 <style>
     .container {
-        height: 100%;
-        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
         display: flex;
         justify-content: center;
         align-items: center;
